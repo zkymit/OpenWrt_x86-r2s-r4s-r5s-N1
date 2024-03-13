147dd28abac160911c40472a78db079a401f333c
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-03-14 06:23:54 +0800

    update

--- devices/rockchip_armv8/diy.sh
+++ devices/rockchip_armv8/diy.sh
@@ -27,6 +27,7 @@ mv -f tmp/r8125 feeds/kiddin9/
 rm -rf target/linux/rockchip/armv8/base-files/etc/uci-defaults/13_opkg_update
 
 sed -i -e 's,kmod-r8168,kmod-r8169,g' target/linux/rockchip/image/armv8.mk
+sed -i -e 's,wpad-openssl,wpad-basic-mbedtls,g' target/linux/rockchip/image/armv8.mk
 
 sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += fdisk lsblk kmod-drm-rockchip/' target/linux/rockchip/Makefile
 
